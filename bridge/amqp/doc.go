96ffba69eddbcecab86da0d461c1871d1068ee86
Hylke Visser <htdvisser@gmail.com>
2016-11-09 15:38:26 +0100

    Add docs for AMQP bridge

---
+++ bridge/amqp/doc.go
@@ -0,0 +1,27 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+// Package amqp connects to an AMQP server in order to communicate with a gateway.
+//
+// Connection/Disconnection of gateways is done by publishing messages to the
+// "connect" and "disconnect" routing keys. When a gateway connects, it (or a
+// plugin on the broker) should publish a types.ConnectMessage to the "connect"
+// routing key (`{"id":"[gateway-id]","token":"[gateway-token]"}``).
+//
+// The gateway (or the plugin) can send a types.DisconnectMessage on the
+// "disconnect" topic (`{"id":"[gateway-id]"}``) when it disconnects, in order
+// to help the bridge clean up connections.
+//
+// Uplink messages are sent as protocol buffers on the "[gateway-id].up" routing
+// key. The bridge should call `SubscribeUplink("gateway-id")` to subscribe to
+// these. It is also possible to subscribe to a wildcard gateway by passing "*".
+//
+// Downlink messages are sent as protocol buffers on the "[gateway-id].down"
+// routing key. The bridge should call `PublishDownlink(*types.DownlinkMessage)`
+// in order to send the downlink to the gateway.
+//
+// Gateway status messages are sent as protocol buffers on the
+// "[gateway-id].status" routing key. The bridge should call
+// `SubscribeStatus("gateway-id")` to subscribe to these. It is also possible to
+// subscribe to a wildcard gateway by passing "*".
+package amqp
