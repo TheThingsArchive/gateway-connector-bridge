c8754174429c8f7b71a54badab77c31cdc82fff7
Hylke Visser <htdvisser@gmail.com>
2017-01-18 13:37:48 +0100

    Add state_test

---
+++ exchange/state_test.go
@@ -0,0 +1,82 @@
+// Copyright © 2016 The Things Network
+// Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+package exchange
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	redis "gopkg.in/redis.v5"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/text"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func getRedisClient() *redis.Client {
+	host := os.Getenv("REDIS_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	return redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:6379", host),
+		Password: "", // no password set
+		DB:       1,  // use default DB
+	})
+}
+
+func TestExchangeState(t *testing.T) {
+	Convey("Given a new Context and Backends", t, func(c C) {
+
+		var logs bytes.Buffer
+		ctx := &log.Logger{
+			Handler: text.New(&logs),
+			Level:   log.DebugLevel,
+		}
+		defer func() {
+			if logs.Len() > 0 {
+				c.Printf("\n%s", logs.String())
+			}
+		}()
+
+		Convey("When creating a new Exchange", func() {
+			b := New(ctx)
+
+			Convey("When calling InitRedisState", func() {
+				gatewayIDs := b.InitRedisState(getRedisClient(), "")
+				Convey("It should not return any gateways", func() {
+					So(gatewayIDs, ShouldBeEmpty)
+				})
+
+				Convey("When adding a gateway", func() {
+					b.gateways.Add("dev")
+					Reset(func() {
+						b.gateways.Remove("dev")
+					})
+					Convey("When calling InitRedisState on another Exchange", func() {
+						gatewayIDs := New(ctx).InitRedisState(getRedisClient(), "")
+						Convey("It should return the gateway", func() {
+							So(gatewayIDs, ShouldContain, "dev")
+						})
+					})
+					Convey("When removing that gateway", func() {
+						b.gateways.Remove("dev")
+						Reset(func() {
+							b.gateways.Add("dev")
+						})
+						Convey("When calling InitRedisState on another Exchange", func() {
+							gatewayIDs := New(ctx).InitRedisState(getRedisClient(), "")
+							Convey("It should not return the gateway", func() {
+								So(gatewayIDs, ShouldNotContain, "dev")
+							})
+						})
+					})
+				})
+			})
+
+		})
+	})
+}
