f1660e692d89cad53758e8ccd37528637c142d9a
Tobias Kaupat <tobiall@niondir.de>
2018-07-27 09:58:44 +0200

    Add default udp connection to ':1700' for bridge and warn if parameter is empty

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -253,6 +253,8 @@ func runBridge(cmd *cobra.Command, args []string) {
 			LockPort: config.GetBool("udp-lock-port"),
 		}, ttnlog.Get())
 		bridge.AddSouthbound(pktfwd)
+	} else {
+		ctx.Warn("Parameter 'udp' is empty. No UDP listener for gateways opened")
 	}
 
 	// Set up the MQTT backends (from comma-separated list of user:pass@host:port)
@@ -373,7 +375,7 @@ func init() {
 	BridgeCmd.Flags().Uint("ratelimit-status", 20, "Status rate limit (per gateway per minute)")
 
 	BridgeCmd.Flags().StringSlice("ttn-router", []string{"discover.thethingsnetwork.org:1900/ttn-router-eu"}, "TTN Router to connect to")
-	BridgeCmd.Flags().String("udp", "", "UDP address to listen on for Semtech Packet Forwarder gateways")
+	BridgeCmd.Flags().String("udp", ":1700", "UDP address to listen on for Semtech Packet Forwarder gateways")
 	BridgeCmd.Flags().Duration("udp-session", time.Minute, "Duration of gateway sessions")
 	BridgeCmd.Flags().Bool("udp-lock-ip", true, "Lock gateways to IP addresses for the session duration")
 	BridgeCmd.Flags().Bool("udp-lock-port", false, "Additional to udp-lock-ip, also lock gateways to ports for the session duration")
